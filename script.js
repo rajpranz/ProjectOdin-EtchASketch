298b9ff19a21bede67e056cbed477766d6b052ac
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-22 00:02:33 +1200

    Dynamically size input blocks

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 var target = document.querySelector(".sketchinput");
 
 function generateInputBlocks(dimension) {
-    let number = dimension*2;
+    
+    let number = dimension*dimension;
+    let boxSize = (600/dimension)-1; //reduce 1px to adjust for margin
+
+    var fragment = document.createDocumentFragment();
 
     for(var i=0; i < number; i++){
         var newDiv = document.createElement('div');
         newDiv.id = 'r'+i;
         newDiv.className = 'block';
-        target.appendChild(newDiv);
-     }
+        newDiv.style = `background-color:red; width:${boxSize}px; height:${boxSize}px;`;
+        fragment.appendChild(newDiv);
+    }
     
+
+    return fragment;
 }
 
-generateInputBlocks(36);
+target.appendChild(generateInputBlocks(6));
 
 
 
