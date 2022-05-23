6aa564d53831924d2ea0a8d5396a57a2761cd784
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-23 20:24:38 +1200

    Get transperancy of clicked input block

--- script.js
+++ script.js
@@ -2,12 +2,33 @@
 var resolution = document.getElementById("blockResolution");
 var mode = document.getElementById("modeSwitch");
 
-
     var target = document.querySelector(".sketchinput");
     target.innerHTML="<div></div>";
     target.appendChild(generateInputBlocks(resolution.value));
     console.log(resolution.value);
 
+    //var inputBlock = document.querySelectorAll(".block");
+    //inputBlock.forEach(addEventListeners);
+
+    function addEventListeners(block){
+        block.addEventListener('click', updateColor);
+        //block.addEventListener('mouseenter', updateColor);
+    }
+
+    function updateColor(block){
+        console.log('clicked');
+        //console.log(e.target.style.backgroundColor);
+        var style = getComputedStyle(block);
+        var color = style.backgroundColor;
+        const parts = color.match(/[\d.]+/g);
+    
+        // If alpha is not there, add it:
+        if (parts.length === 3) {
+            parts.push(1);
+        }
+
+        console.log("transperancy = " + parts[3]);
+    }
 
 
 function generateInputBlocks(dimension) {
@@ -22,6 +43,12 @@ function generateInputBlocks(dimension) {
         newDiv.id = 'r'+i;
         newDiv.className = 'block';
         newDiv.style = `width:${boxSize}px; height:${boxSize}px;`;
+        newDiv.setAttribute('onmouseenter', 'updateColor(this)');
+        newDiv.setAttribute('onclick', 'updateColor(this)');
+        newDiv.addEventListener('click', function onClick(event){
+           // console.log(event.target.style.backgroundColor);
+        });
+        
         fragment.appendChild(newDiv);
     }
     
